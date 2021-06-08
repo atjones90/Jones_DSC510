e913b96dea183e8f977603d99a897d22e55a2516
avarma57319 <arvarma@my365.bellevue.edu>
2021-06-08 07:00:40 -0400

    Revert "Create VARMA_DSC510_ASSIGNMENT1.1.py"
    This reverts commit 47c3f435e4e1c16610b75bdb2d0ee4491efc49f0.

--- VARMA_DSC510/VARMA_DSC510_ASSIGNMENT1.1.py
+++
@@ -1,7 +0,0 @@
-# Assignment 1.1 DSC-510: Hello World Program
-# Author: Arjun Varma
-# Created Date: 06-08-2021
-# This program, once executed will print out the string: Hello World on the output console
-
-print("Hello World")
-
