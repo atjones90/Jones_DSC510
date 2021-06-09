5e1bdb93690e50c3d21c4f1d7544c9987d459bbb
MrKimhoff <timhof@my365.bellevue.edu>
2021-06-08 20:09:42 -0500

    added input function to retrieve name from user

--- IMHOF_DSC510/hello.py
+++ IMHOF_DSC510/hello.py
@@ -1,8 +1,9 @@
 # Taylor Imhof
 # Bellevue University | DSC 510
 # Date Created: 6.7.2021
-# Last Update:  6.7.2021
+# Last Update:  6.8.2021
 
 # hello.py
 # this program simply displays the string 'Hello, world!' to the user's console
-print('Hello, world!')
+name = input('What is your name?\n')
+print('Hello,', name)
