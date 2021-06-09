5853e1b1f908cd982d30552e7cac231eecceccbc
SharathChnadraTummanapally <sctummanapally.my365@bellevu.edu>
2021-06-09 17:44:21 -0500

    Hello World Program

--- TUMMANAPALLY_DSC510/HelloWorld.py
+++ TUMMANAPALLY_DSC510/HelloWorld.py
@@ -1,3 +1,8 @@
-# Week 1 Assignment 1.2
+# DSC510
+# Week 1
+# Programming Assignment 1.2
+# Author Sharath Chandra Tummanapally
+# 6/8/2021
+
 # This is the script for Hello World Program.
 print('Hello World')
