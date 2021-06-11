cf95d272e8e896b294e8e0dec1717d0804c1fffd
MrKimhoff <timhof@my365.bellevue.edu>
2021-06-10 22:24:05 -0500

    updated changelog and documentation to reflect changes

--- IMHOF_DSC510/hello.py
+++ IMHOF_DSC510/hello.py
@@ -1,9 +1,15 @@
 # Taylor Imhof
 # Bellevue University | DSC 510
 # Date Created: 6.7.2021
-# Last Update:  6.8.2021
+# Last Update:  6.10.2021
+
+# Change Log:
+#   6.8.2021    -- Added functionality to accept user's name from console and display short greeting
+#   6.10.2021   -- Updated documentation to reflect changes
 
 # hello.py
-# this program simply displays the string 'Hello, world!' to the user's console
-name = input('What is your name?\n')
-print('Hello,', name)
+# this program first displays the string "Hello, world!" to the console
+# then, the user is prompted for their name and a short greeting is then display to the console
+print('Hello, world!\n')
+name = input('What is your name, stranger?\n')
+print(f"Well, top of the mornin' ta ya, {name}!")
