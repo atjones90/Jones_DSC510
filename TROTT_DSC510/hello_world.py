75df15f3b7d8a58c6d9f0eec090516fa3b3bbd89
Dustin Trott <dtrott@my.365.bellevue.edu>
2021-06-12 21:51:17 -0500

    hello_world

---
+++ TROTT_DSC510/hello_world.py
@@ -0,0 +1,10 @@
+"""
+DSC 510
+Week 1
+Programming Assignment 1.2
+Author: Dustin Trott
+Create Date: 6/12/21
+This program, once executed will print out the string: Hello World on the output console
+"""
+
+print("Hello World")
